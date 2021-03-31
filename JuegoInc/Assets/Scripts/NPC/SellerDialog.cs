63741e6c62553804a04eda75e267438054f7a6e0
Efrain Quiñonez Lopez <efrainlp1995@gmail.com>
2021-03-30 20:55:38 -0500

    Biblioteca falta colisiones
    Colisiones del seller y player

---
+++ JuegoInc/Assets/Scripts/NPC/SellerDialog.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SellerDialog : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public Text dialogText;
+    public string dialog;
+    public bool playerInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) || playerInRange)
+        {
+            if (dialogBox.activeInHierarchy)
+            {
+                dialogBox.SetActive(false);
+            }
+            else
+            {
+                dialogBox.SetActive(true);
+                dialogText.text = dialog;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("choco");
+        if (other.CompareTag("Seller")) 
+        {
+            //Debug.Log("choco");
+            playerInRange = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("salio");
+        if (other.CompareTag("Player"))
+        {
+            //Debug.Log("salio");
+            playerInRange = false;
+            dialogBox.SetActive(false);
+        }
+    }
+}
---
+++ JuegoInc/Assets/Scripts/Player/Character.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private Animator animator;
+    protected Vector2 direction;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        move();
+    }
+
+    public void move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        moveAnimate(direction);
+    }
+
+    public void moveAnimate(Vector2 direction) 
+    {
+        animator.SetFloat("x",direction.x);
+        animator.SetFloat("y", direction.y);
+    }
+}
